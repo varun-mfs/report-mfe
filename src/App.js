7ed93d3acfebf16588bdc497a4d878ababcef9ed
varun <varun.sharma@mindfiresolutions.com>
2023-05-26 22:35:15 +0530

    add newCount API call by subscribing to clickedNews event

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
+import WindowEventService from "news_layout/PubSub";
 
 import "./index.css";
 
 const App = () => {
 
+  // TODO: REMOVE this code:
+  // TEST: Firing an EVENT to which you subscribed
+  // const obj = {
+  //   id: 1234
+  // }
+
+  // WindowEventService.fire("clickedNews", obj);
+  // setTimeout(() => {
+  //   console.log("inside setimeout");
+  //   WindowEventService.fire("clickedNews", obj);
+  // }, 1);
+
+  useEffect(() => {
+    console.log("first", WindowEventService);
+    const requestOptions = {
+      method: 'PUT',
+      // headers: { 'Content-Type': 'application/json' },
+      // body: JSON.stringify({ title: 'React PUT Request Example' })
+    };
+
+    WindowEventService.subscribe("clickedNews", async () => { console.log("successfully Subscribed!"); await increaseNewsReadCount(); });
+    
+    async function increaseNewsReadCount() {
+      const apiUrl = 'https://jsonplaceholder.typicode.com/todos/1';
+      const response = await fetch(apiUrl, requestOptions);
+      let data = await response.json();
+      console.log(data);
+    }
+    
+    
+    console.log("last");
+    return () => {
+      WindowEventService.unsubscribe("clickedNews", () => { console.log("successfully unsubscribed!") });
+    }
+  }, []);
 
   return (
     <div className="container">
